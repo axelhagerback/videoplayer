caf1d2b209aeef90d591b12d58df17c4cc154212
CasperHogstrom <casperhogstrom3@gmail.com>
2023-02-07 22:51:58 +0100

    Changed colors and rounded the headers bottom

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,9 @@ import Dropdown from "./Dropdown.vue";
 </script>
 
 <template>
-    <div class=" w-full h-24 bg-teal-700">
-        <input type="text" placeholder="URL" class="w-3/6 float-left m-8 outline outline-2 outline-white bg-teal-700 rounded-lg indent-[6px] text-white">
-        <input type="button" value="Add"  class="w-11 float-left my-8 outline outline-2 outline-white bg-teal-800 rounded-lg indent-[1px] text-white">
+    <div class=" w-full h-24 bg-red-500 rounded-br-lg rounded-bl-lg">
+        <input type="text" placeholder="URL" class="w-3/6 float-left m-8 outline outline-2 outline-white bg-red-600 rounded-lg indent-[6px] text-white">
+        <input type="button" value="Add"  class="w-11 float-left my-8 outline outline-2 outline-white bg-red-600 rounded-lg indent-[1px] text-white">
 
         <Dropdown />
 
