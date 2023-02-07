a09084e0ca2497a6bba63b4af6231b1cd88c72d4
elliotabrahamsson <elliotsson@outlook.com>
2023-02-07 14:23:56 +0100

    Xhr get video list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,35 @@
+<script setup></script>
+
+<script>
+export default {
+  mounted: {
+    getVideo() {
+      const XHR = new XMLHttpRequest();
+
+      XHR.onload = (res) => {
+        let video = [];
+        video.forEach((videos) => {
+          video.push(videos);
+        });
+      };
+
+      XHR.open("GET", "/video");
+      XHR.send();
+    },
+  },
+  methods: {
+    addVideo() {
+      XHR.open("POST", "render");
+      XHR.send("link");
+    },
+    showVideo() {
+      XHR.open("GET", `/video/${id}`);
+      XHR.send();
+    },
+  },
+};
+</script>
+
 <template>
   <p>This is the home view</p>
 </template>
